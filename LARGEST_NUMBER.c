8e04047eaa213102da44df243d02d6de5b390049
raj-lab-pixel <r9397387@gmail.com>
2025-12-11 15:48:02 +0530

    Update LARGEST_NUMBER.c

--- LARGEST_NUMBER.c
+++ LARGEST_NUMBER.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
-int findLargest(int arr[], int size) { 
-    int max = arr[0];                 
-    for (int i = 1; i < size; i++) {   
-        if (arr[i] > max)              
-            max = arr[i];              
+#include<stdio.h>
+int findLargest(int arr[],int size){ 
+    int max=arr[0];                 
+    for(int i=1;i<size;i++){   
+        if(arr[i]>max)              
+            max=arr[i];              
     }
     return max;
 }
-int main() {
+int main(){
     int size;
-    printf("Enter array size: ");
-    scanf("%d", &size);                
+    printf("enter array size: ");
+    scanf("%d",&size);                
     int arr[size];                     
-    printf("Enter %d elements:\n", size);
-    for (int i = 0; i < size; i++)     
-        scanf("%d", &arr[i]);
-    int largest = findLargest(arr, size);   
-    printf("Largest element = %d", largest);
+    printf("enter %d elements:\n",size);
+    for (int i=0;i<size;i++)     
+        scanf("%d",&arr[i]);
+    int largest=findLargest(arr,size);   
+    printf("largest element=%d",largest);
     return 0;
 }
+
